e5966b7548d9c6dd104d87371a7b79ddc0266a96
Ryan Hausen <rhausen@ucsc.edu>
2016-07-14 00:10:50 -0700

    Refactored segmap code, so that it would be usable from main.py Cleaned up main.py code a little

--- src/main.py
+++ src/main.py
@@ -1,29 +1,16 @@
 # -*- coding: utf-8 -*-
-# this file tests dropping other sources from a file and occupying there
-# space with gaussian noise fitted to the noise of the image.
-# test files 
-# GDS_deep2_3518_segmap.fits
-# GDS_deep2_3518_z.fits
-# GDS_deep2_3518_v.fits
-# GDS_deep2_3518_j.fits
-# GDS_deep2_3518_h.fits
 
 # imports
 import numpy as np
-from numpy.random import normal, randint
+from numpy.random import randint
 from astropy.io import fits
-from sklearn.preprocessing import MinMaxScaler
-
+from segmap import transform_segmap
+from copy import deepcopy
 
 # FLAGS
 # set to true to replace all pixels except central source
 # set to false to replace only the pixels in non-central source segmaps
 replace_all_pixels = True
-
-# set to true to draw pixel values from a assumed nice gaussian based on noise
-# set to false to replace pixel values with ones randomly drawn from the noise
-use_gaussian = False
-
 # END FLAGS
 
 
@@ -32,114 +19,69 @@ img_dir = '../data/jeyhan/gds2/GDS_deep2_1/'
 output_dir = '../data/'
 
 files = {'segmap':'GDS_deep2_{}_segmap.fits',
-         'img_z': 'GDS_deep2_{}_z.fits',
-         'img_v': 'GDS_deep2_{}_v.fits',
-         'img_j': 'GDS_deep2_{}_j.fits',
-         'img_h': 'GDS_deep2_{}_h.fits'}
+         'z': 'GDS_deep2_{}_z.fits',
+         'v': 'GDS_deep2_{}_v.fits',
+         'j': 'GDS_deep2_{}_j.fits',
+         'h': 'GDS_deep2_{}_h.fits'}
 img_data = {}
 
-img_id = 3518
+img_id = 3882
 
 for f_type in files.iterkeys():
     file_dir = img_dir + files[f_type].format(img_id)
-    img_data[f_type] = fits.open(file_dir)[0].data
+    img_data[f_type] = fits.getdata(file_dir)
+
+transformed_segmap = transform_segmap([img_data['z'],img_data['v'],img_data['j'],img_data['h']],
+                                     img_data['segmap'])
+
+
 
 # get non source pixel locations
 non_src_mask = img_data['segmap'] == 0
+transformed_mask = transformed_segmap == 0
 
 for img_key in img_data.iterkeys():
     if img_key != 'segmap':
-        img = img_data[img_key]        
-        noise = img[non_src_mask]
+        img = img_data[img_key]     
+        noise = img[non_src_mask]  
+        len_noise = len(noise)
         
-        #img_min = np.min(img)
-        #img_max = np.max(img)        
+        trans_img = deepcopy(img)               
+        trans_noise = trans_img[transformed_mask]
+        len_trans_noise = len(trans_noise)
         
-        mu = None
-        sigma = None
-        len_noise = None    
-        
-        
-        if use_gaussian:
-            mu = np.mean(noise)
-            sigma = np.sqrt(np.var(noise))
-        else:
-            len_noise = len(noise)
         
         rpl_src_mask = None
+        trans_rpl_src_mask = None
         if replace_all_pixels:
             rpl_src_mask = img_data['segmap'] != img_id
+            trans_rpl_src_mask = transformed_segmap != img_id
         else:
             rpl_src_mask = np.logical_and(img_data['segmap'] > 0,
                                           img_data['segmap'] != img_id)
+            
+            trans_rpl_src_mask = np.logical_and(transformed_segmap > 0,
+                                                transformed_segmap != img_id)
         
         for i in xrange(img.shape[0]):
             for j in xrange(img.shape[1]):
                 if rpl_src_mask[i,j]:
-                    
-                    if use_gaussian:                    
-                        img[i,j] = normal(loc=mu, scale=sigma)
-                    else:
-                        img[i,j] = noise[randint(len_noise)]                    
-                    
-                    # idea for using averaging for filling in segmaps                    
-                    
-                    #prev_i = i - 1
-                    #prev_j = j - 1
-                    #post_i = i + 1
-                    #post_j = j + 1
+                    img[i,j] = noise[randint(len_noise)]                    
+                  
+                if trans_rpl_src_mask[i,j]:
+                    trans_img[i,j] = trans_noise[randint(len_trans_noise)]
+                  
+                  
                 
-                    #i_m1 = i - 1 >= 0
-                    #j_m1 = j - 1 >= 0
-                                 
-                    #i_p1 = i + 1 >= 84
-                    #j_p1 = j + 1 >= 84
-                    
-
-                    #pxl_smpl = 0.0
-                    #pxl_count = 0            
-                    
-                    #if i_m1:
-                    #    pxl_smpl += img[prev_i, j]
-                    #    pxl_count += 1
-                        
-                    #    if j_m1:
-                    #        pxl_smpl += img[prev_i, prev_j]
-                    #        pxl_count += 1
-                    #    if j_p1:
-                    #        pxl_smpl += img[prev_i, post_j]
-                    #        pxl_count += 1
-                        
-                    #if i_p1:
-                    #    pxl_smpl += img[post_i, j]
-                    #    pxl_count += 1
-                        
-                    #    if j_m1:
-                    #        pxl_smpl += img[post_i, prev_j]
-                    #       pxl_count += 1
-                    #    if j_p1:
-                    #        pxl_smpl += img[post_i, post_j]
-                    #        pxl_count += 1
-                            
-                    #if j_m1:
-                    #    pxl_smpl += img[i, prev_j]
-                    #    pxl_count += 1
-                    
-                    #if j_p1:
-                    #    pxl_smpl += img[i, post_j]
-                    #    pxl_count += 1
-                        
-                    #avg_smpl = pxl_smpl / float(pxl_count)
-                    #add_noise = normal(loc=0.0, scale=sigma)
-                    
-                    #img[i,j] = avg_smpl + add_noise
-                                        
-                    
-        #MinMaxScaler(feature_range=(img_min, img_max), copy=False).fit_transform(img)
-        
-                                  
-        # save new file
-        file_dir = output_dir + files[img_key].format(img_id)
-                                  
+        file_dir = output_dir + 'ORIG_' + files[img_key].format(img_id)
+         
         hdu = fits.PrimaryHDU(img)
         hdu.writeto(file_dir)
+        
+        trans_dir = output_dir + 'NEW_' + files[img_key].format(img_id)       
+        
+        hdu = fits.PrimaryHDU(trans_img)
+        hdu.writeto(trans_dir)
+
+seg_dir = output_dir + 'transformed_segmap'
+fits.PrimaryHDU(transformed_segmap).writeto(seg_dir)
--- src/segmap.py
+++ src/segmap.py
@@ -1,27 +1,16 @@
 # -*- coding: utf-8 -*-
-# create twos sets segmaps based on each filter given an img_id
-# 1st segmap is based on the original image (orig)
-# 2nd segmap is based on a gaussian blur sigma=2 (blur)
-#
-# The segmaps in the output are labeled with ORIG and BLUR
-#
 # based on : # http://scikit-image.org/docs/dev/auto_examples/applications/plot_coins_segmentation.html
 #
 
 
 import numpy as np
-from astropy.io import fits
 from scipy.ndimage.filters import gaussian_filter
 from skimage.filters import sobel
 from skimage import morphology
 from math import hypot
 
 # helpers ----------------------------------------------------
-def hist_dict(img):
-    y, x = np.histogram(img, bins=100)
-    return {'x':x, 'y':y}
-
-def get_split_idx(hist_y):
+def _get_split_idx(hist_y):
     hist_diff = np.diff(hist_y)
     max_val = np.max(hist_diff)    
     
@@ -41,120 +30,61 @@ def get_split_idx(hist_y):
             if val >= 0:
                 return i
                 
-# for one of the libraries, i think sobel images have to be between -1 and 1
-def cap_at_1(img):
+# for sobel images have to be between -1 and 1
+def _cap_at_1(img):
     img[img > 1] = 1
     return img
         
 # ------------------------------------------------------------
 
-img_dir = '../data/jeyhan/gds2/GDS_deep2_1/'
-output_dir = '../data/'
 
-segmap_file = 'GDS_deep2_{}_segmap.fits'
-
-files = {'img_z': 'GDS_deep2_{}_z.fits',
-         'img_v': 'GDS_deep2_{}_v.fits',
-         'img_j': 'GDS_deep2_{}_j.fits',
-         'img_h': 'GDS_deep2_{}_h.fits'}
-
-orig_img_data = {}
-blur_img_data = {}
-
-orig_maps = {}
-blur_maps = {}
-
-orig_hists = {}
-blur_hists = {}
-
-orig_markers = {}
-blur_markers = {}
-
-orig_segmaps = {}
-blur_segmaps = {}
-
-# 3518
-img_id = 3350
-
-union_segmap = np.zeros((84,84))
-
-for f_type in files.iterkeys():
-    # get image data  
-    file_dir = img_dir + files[f_type].format(img_id)
-    orig = cap_at_1(fits.getdata(file_dir))    
-    blur = gaussian_filter(orig, sigma=2)
+def transform_segmap(imgs, segmap, sigma=2, truncate=4.0):
+    """
+    imgs : list of img in different bands as numpy arrays 
+    segmap : the segmap from which labels can be drawn
+    sigma : passed as sigma to scipy.ndimage.filters.gaussian_filter     
+    truncate: passed as truncate to scipy.ndimage.filters.gaussian_filter
     
-    orig_img_data[f_type] = orig
-    blur_img_data[f_type] = blur
-    
-    # get sorbel gradient map
-    orig_map = sobel(orig)
-    orig_maps[f_type] = orig_map
-    
-    blur_map = sobel(blur)
-    blur_maps[f_type] = blur_map
-    
-    # get histogram
-    orig_hist = hist_dict(orig)
-    orig_hists[f_type] = orig_hist
-    
-    blur_hist = hist_dict(blur)
-    blur_hists[f_type] = blur_hist
-    
-    orig_split_idx = get_split_idx(orig_hist['y'])
-    blur_split_idx = get_split_idx(blur_hist['y'])    
-    
-    # get markers
-    o_x = orig_hist['x']
-    orig_marker = np.zeros_like(orig)
-    orig_marker[orig < o_x[orig_split_idx]] = 1
-    orig_marker[orig > o_x[orig_split_idx + 1]] = 2
-    
-    orig_markers[f_type] = orig_marker
+    Uses Gaussian to blur input images and broaden passed in segmap
+    """
+    union_segmap = np.zeros_like(segmap)
 
-    b_x = blur_hist['x']
-    blur_marker = np.zeros_like(blur)
-    blur_marker[blur < b_x[blur_split_idx]] = 1
-    blur_marker[blur > b_x[blur_split_idx + 1]] = 2
-    
-    blur_markers[f_type] = blur_marker  
-    
-    orig_segmap = morphology.watershed(orig_map, orig_marker)
-    blur_segmap = morphology.watershed(blur_map, blur_marker)   
-    
-     # save new file
-    #orig_file_dir = output_dir + 'ORIG_NEW_SEG-' + files[f_type].format(img_id)
-    blur_file_dir = output_dir + 'BLUR_NEW_SEG-' + files[f_type].format(img_id)
-                                  
-    #fits.PrimaryHDU(orig_segmap).writeto(orig_file_dir)
-    fits.PrimaryHDU(blur_segmap).writeto(blur_file_dir)
-    
-    # union all blurs
-    # union_segmap = cap_at_1(union_segmap + ((orig_segmap-1) + (blur_segmap-1)))
-    union_segmap = cap_at_1(union_segmap + (blur_segmap-1))
-
-# merge with original segmap
-given_segmap = fits.getdata(img_dir + segmap_file.format(img_id))
-
-for i in range(84):
-    for j in range(84):
-        # if the pixel has a value in the given segmap then give it that value
-        if given_segmap[i,j] > 0:
-            union_segmap[i,j] = given_segmap[i,j]
-        # if our new segmap has a value that isn't in the given segmap then
-        # find the closest label and assign it 
-        elif union_segmap[i,j] > 0:
-            # NAIVE IMPLEMENTATION FIND A BETTER ALGORITHM!!!!!
-            coords = None
-            min_dist = hypot(84,84)
-            for k in range(84):
-                for l in range(84):
-                    if given_segmap[k,l] > 0 and hypot(i-k, j-l) < min_dist:
-                        coords = (k,l)
-                        min_dist = hypot(i-k, j-l)
-                        
-            union_segmap[i,j] = given_segmap[coords[0], coords[1]]
+    # create blurred segmaps  
+    for img in imgs:
+        img = gaussian_filter(_cap_at_1(img), sigma=sigma, truncate=truncate)
+        img_map = sobel(img)
+        y, x = np.histogram(img, bins=100)
         
-union_segmap[union_segmap == 1] = img_id
-union_dir = output_dir + 'UNION_SEG_{}.fits'.format(img_id)
-fits.PrimaryHDU(union_segmap).writeto(union_dir)
+        split_idx = _get_split_idx(y)
+        markers = np.zeros_like(img)
+        markers[img < x[split_idx]] = 1
+        markers[img > x[split_idx + 1]] = 2
+        
+        new_segmap = morphology.watershed(img_map, markers)
+        
+        union_segmap = _cap_at_1(union_segmap + (new_segmap-1))
+
+    # merge and label with passed in segmap
+    dim1 = segmap.shape[0]
+    dim2 = segmap.shape[1]
+
+    for i in range(dim1):
+        for j in range(dim2):
+            # if the pixel has a value in the given segmap then give it that value
+            if segmap[i,j] > 0:
+                union_segmap[i,j] = segmap[i,j]
+            # if our new segmap has a value that isn't in the given segmap then
+            # find the closest label and assign it 
+            elif union_segmap[i,j] > 0:
+                # NAIVE IMPLEMENTATION FIND A BETTER ALGORITHM!!!!!
+                coords = None
+                min_dist = hypot(dim1,dim2)
+                for k in range(dim1):
+                    for l in range(dim2):
+                        if segmap[k,l] > 0 and hypot(i-k, j-l) < min_dist:
+                            coords = (k,l)
+                            min_dist = hypot(i-k, j-l)
+                            
+                union_segmap[i,j] = segmap[coords[0], coords[1]]
+
+    return union_segmap
