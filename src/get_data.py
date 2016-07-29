c848e97cf1c8200c2dd691012d92cc324005c7ae
Ryan Hausen <rhausen@ucsc.edu>
2016-07-29 00:45:03 -0700

    Broken. Need to debug cropping code.

--- src/get_data.py
+++ src/get_data.py
@@ -80,6 +80,90 @@ def _simple_log(val):
     with open('./log', 'a') as f:
         f.write(val + '\n')
 
+# if an image is larger than 84x84 we need to see if we can crop it down
+def _fits_in_square(seg, src_id):
+    dim1, dim2 = np.shape(seg)    
+    
+    
+    min_1, max_1 = (dim1+1,-1)
+    min_2, max_2 = (dim2+1,-1)
+    
+    for d1 in range(dim1):
+        for d2 in range(dim2):
+            if seg[d1,d2] == src_id:
+                if d1 < min_1:
+                    min_1 = d1
+                elif d1 > max_1:
+                    max_1 = d1
+                
+                if d2 < min_2:
+                    min_2 = d2
+                elif d2 > max_2:
+                    max_2 = d2
+                    
+    diff_1 = max_1 - min_1
+    diff_2 = max_2 - min_2
+    # the image is too big too fit within our constraints
+    if diff_1 > 83 or diff_2 > 83:
+        return None
+    # pad the area to make it 84x84
+    else:
+        def parse_diffs(diff, mn, mx):
+            if diff % 2 == 0:
+                pad = diff / 2
+    
+                mx_too_big = mx + pad > 83
+                mn_too_small = mn - pad < 0
+    
+                # we need to know which order to adjust the pad in
+    
+                if mn_too_small:
+                   pad += (pad - mn) * (-1)
+                   mn = 0
+                   
+                   mx += pad
+                   
+                elif mx_too_big:
+                   pad += (pad + mx) - 83
+                   mx = 83
+                   
+                   mn -= pad
+                
+                else:
+                    mn -= pad
+                    mx += pad
+                
+            else:
+                pad_1, pad_2 = (diff / 2, diff / 2 + 1)
+                
+                mx_too_big = mx + pad_2 > 83
+                mn_too_small = mn - pad_1 < 0
+                
+                if mn_too_small:
+                    pad_2 += (pad_1 - mn) * (-1)
+                    mn = 0
+                    
+                    mx += pad_2
+                elif mx_too_big:
+                    pad_1 += (pad_2 + mx) - 83
+                    mx = 83
+                    
+                    mn -= pad_1
+                else:
+                    mn -= pad_1
+                    mx += pad_2
+            
+            return (mn, mx)
+            
+        min_1, max_1 = parse_diffs(diff_1, min_1, max_1)
+        min_2, max_2 = parse_diffs(diff_2, min_2, max_2)
+        
+        
+            
+        
+        return (min_1, max_1, min_2, max_2)
+        
+    
 # Helpers ---------------------------------------------------------------------
 
 
@@ -93,11 +177,12 @@ data_dir = _new_dir('../','data')
 # tmp dir for the raw files
 tmp_dir = _new_dir(data_dir, 'tmp')
 
-# put all the raw data in the tmp directory
-_get_ftp(data_ftp_url, tmp_dir, path=data_ftp_dir)
-
-# unpack all the tarballs
-_unpack_dir(tmp_dir)
+if len(os.listdir(tmp_dir)) == 0:
+    # put all the raw data in the tmp directory
+    _get_ftp(data_ftp_url, tmp_dir, path=data_ftp_dir)
+    
+    # unpack all the tarballs
+    _unpack_dir(tmp_dir)
 
 # get the distinct image names
 sources =  _distinct_sources(tmp_dir)
@@ -119,10 +204,50 @@ for s in sources.iterkeys():
     img_count += 1
     
     s_dir = sources[s]
+    img_id = int(s[(s.index('_')+1):])
 
     # fix segmap
     seg_dir = os.path.join(s_dir, f_format.format(s, 'segmap'))
     segmap = fits.getdata(seg_dir)
+
+    valid_img = True
+    pad = False
+    crop = False
+    crop_help = None
+    pad_help = None    
+    
+    # if our image is not 84x84
+    dim = np.shape(segmap)[0]
+    if dim != 84:
+        # we need pad the edges to pad the with the correct noise will 
+        # add '1' pixels to emulate a non central source
+        if dim < 84:
+            pad = True
+
+            pad_help = 84 - dim
+
+            d1_pad = np.ones((pad_help,dim))
+            d2_pad = np.ones((84,pad_help))
+            
+            segmap = np.append(segmap, d1_pad)
+            segmap = np.append(segmap, d2_pad)
+            
+            
+            
+        else:
+            crop_help = _fits_in_square(segmap,img_id)
+            if crop_help:
+                crop = True
+                
+                d1_mn, d1_mx, d2_mn, d2_mx = crop_help                
+                segmap = segmap[d1_mn:d1_mx, d2_mn:d2_mx]
+            else:
+                valid_img = False
+    
+    if not valid_img:
+        err = '{} skipped: dimensions are too large and the segmap will not fit '
+        _simple_log(err.format(s))
+        continue
 
     imgs = {}
     try:
@@ -130,9 +255,22 @@ for s in sources.iterkeys():
             i_dir = os.path.join(s_dir, f_format.format(s, b))
             raw_img = fits.getdata(i_dir)                        
             
-            # some imgs are bigger than 84x84 so we shrink them            
-            
-            
+            if crop:
+                _simple_log('cropping {} band {}'.format(s,b))
+                d1_mn, d1_mx, d2_mn, d2_mx = crop_help
+                # crop image 
+                raw_img = raw_img[d1_mn:d1_mx, d2_mn:d2_mx]                
+            elif pad:
+                # pad with 0's it doesn't matter what the value is, as the 
+                # img transformer will replace it with random noise
+                _simple_log('padding {} band {}'.format(s, b))                
+                
+                d1_pad = np.ones((pad_help,dim))
+                d2_pad = np.ones((84,pad_help))
+                
+                raw_img = np.append(d1_pad)
+                raw_img = np.append(d2_pad)
+
             imgs[b] = raw_img
     except Exception:
         err = '{} not included because all filters could not be loaded'
