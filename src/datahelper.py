Lost of changes, fixes for noise issues in image_helper, progress on global average pooling in resnet (still shows nan for some reason, but dims are correct), data helper can iterate a single image now.

src/datahelper.py
@@ -48,34 +48,37 @@ class DataHelper(object):
         self.training = True        
         self.testing = False        
 
-        size_change = False        
-        num_train_examples = int(len(self._imgs_list) * train_size)
-        
-        batch_train = num_train_examples % batch_size
-        if batch_train != 0:
-            size_change = True
+        # work in batches
+        if batch_size:        
+            num_train_examples = int(len(self._imgs_list) * train_size)
             
-            if batch_train > batch_size / 2:
-                num_train_examples += batch_train
-            else:
-                num_train_examples -= batch_train
-        
-        if size_change:
-            msg = 'Batch didnt divide evenly into training examples ' + \
-                  ' adjusted training size from {} to {}'
-            print msg.format(train_size, float(num_train_examples) /  float(len(self._imgs_list)))
-        
-        # we want to use the same test images every time so they are set 
-        # aside before the shuffle
-        self._train_imgs = self._imgs_list[:num_train_examples]        
-        self._test_imgs = self._imgs_list[num_train_examples:]
-        
-        if len(self._train_imgs) % batch_size != 0:
-            err = 'Batch size must divide evenly into training. Batch: {} Train size: {}'
-            raise Exception(err.format(batch_size, len(self._train_imgs)))
-        
-        if shuffle_train:
-            shuffle(self._train_imgs)
+            batch_train = num_train_examples % batch_size
+            if batch_train != 0:
+                
+                if batch_train > batch_size / 2:
+                    num_train_examples += batch_train
+                else:
+                    num_train_examples -= batch_train
+            
+                msg = 'Batch didnt divide evenly into training examples ' + \
+                      ' adjusted training size from {} to {}'
+                print msg.format(train_size, float(num_train_examples) /  float(len(self._imgs_list)))
+            
+            # we want to use the same test images every time so they are set 
+            # aside before the shuffle
+            self._train_imgs = self._imgs_list[:num_train_examples]        
+            self._test_imgs = self._imgs_list[num_train_examples:]
+            
+            if len(self._train_imgs) % batch_size != 0:
+                err = 'Batch size must divide evenly into training. Batch: {} Train size: {}'
+                raise Exception(err.format(batch_size, len(self._train_imgs)))
+            
+            if shuffle_train:
+                shuffle(self._train_imgs)
+                
+        # one example at a time
+        else:
+            self.testing = True
         
     def _augment_image(self, img, img_id):    
 
@@ -240,7 +243,55 @@ class DataHelper(object):
             
             return (x, y)
             
+    def get_class_distribution(self):
+        cls_count_train = np.zeros([5,])
+        cls_count_test = np.zeros([5,])
+        
+      
+        for i in range(len(self._train_imgs)):
+            src = self._train_imgs[i]
+            s_id = 'GDS_' + src[:-5]
+            lbl = self._lbls.loc[self._lbls['ID']==s_id, self._lbl_cols]
+            lbl = lbl.values.reshape(self._num_classes)
             
+            cls_count_train[np.argmax(lbl)] += 1
+            
+        for i in range(len(self._test_imgs)):
+            src = self._test_imgs[i]
+            s_id = 'GDS_' + src[:-5]
+            lbl = self._lbls.loc[self._lbls['ID']==s_id, self._lbl_cols]
+            lbl = lbl.values.reshape(self._num_classes)
+            
+            cls_count_test[np.argmax(lbl)] += 1
+            
+        return (cls_count_train, cls_count_test)
+
+    def get_next_example(self):
+        source = self._imgs_list[self._idx]
+        self._idx += 1
+        
+        if self._idx >= len(self._imgs_list):
+            self.testing = False
+        
+        bands = ['h','j','v','z']
+        
+        x = fits.getdata(os.path.join(self._imgs_dir, source))
+        
+        tmp_x = []
+        for i in range(4):
+            if bands[i] in self._bands:
+                tmp_x.append(x[:,:,i])
+        
+        x = np.dstack(tmp_x).reshape(1,84,84,len(self._bands))
+        
+        
+        y = self._lbls.loc[self._lbls['ID']=='GDS_'+ source[:-5], self._lbl_cols]
+        y = y.values.reshape(self._num_classes)
+        
+        
+        return (x,y)
+
+       
     # Helper 
     @staticmethod
     def _rescale_label(y):

src/image_helper.py
@@ -1,6 +1,5 @@
 # python libs
 import os
-from copy import deepcopy
 import fcntl
 
 # third party libs
@@ -27,12 +26,11 @@ def transform_image(img, img_id, src_name, band, segmap, tinytim):
     # if there isn't any noise in the image, the there isn't anything for us
     # to replace, wide2_9682 is an example of this
     if len_noise > 0:
-        cpy_noise = deepcopy(img)    
+        cpy_noise = np.zeros(img.shape)    
         
         for i in xrange(img.shape[0]):
             for j in xrange(img.shape[1]): 
-                if segmap[i,j] > 0:
-                    cpy_noise[i,j] = noise[randint(len_noise)]
+                cpy_noise[i,j] = noise[randint(len_noise)]
     
     
          # resize and rescale the tiny tim image
@@ -68,15 +66,7 @@ def transform_image(img, img_id, src_name, band, segmap, tinytim):
         for i in xrange(img.shape[0]):
             for j in xrange(img.shape[1]): 
                 if repl_mask[i,j]:
-                    # the h and j bands appear to have more spatially correlated
-                    # noise so we use the tiny tim convoluted noise here
-                    # the v and z bands however have noise that is less spatially
-                    # correlated so we'll just a random draw from the existing 
-                    # noise to replace those pixels
-                    if band in ('h','j'):                  
-                        img[i,j] = tt_img[i,j]
-                    else:
-                        img[i,j] = noise[randint(len_noise)]
+                    img[i,j] = tt_img[i,j]
                         
 
     return img    

src/main.py
@@ -5,33 +5,33 @@ from math import sqrt
 import numpy as np
 import time
 import colorama 
-
+import cPickle
 
 import tensorflow as tf
 
 colorama.init(autoreset=True)
 
 # PARAMS
-batch_size = 93
+batch_size = 229
 train_size = 0.8
 n_classes = 5
 decay_steps = 150
 decay_base = 0.96
-epoch_reduce = None
+epoch_reduce = None#[100]
 # used to be .0001
 start_learning_rate = .1
 momentum = 0.9
-bands_to_use = ['v','z']
-block_config = [3,9,27]
+bands_to_use = ['v','z','h','j']
+block_config = [3,8,36,3]
 trans_func = None#lambda x: np.log10(x + 1.0)
 band_trans_func = None#lambda x: (((1-.0001)*(x-np.min(x)))/(np.max(x)-np.min(x))) + .0001
-label_noise_stddev = 0.01
+label_noise_stddev = 0.001
 
 display_step = 10
 model_dir = '../models/'
 train_progress = '../report/train_progress.csv'
 test_progress = '../report/test_progress.csv'
-save_progress = False
+save_progress = True
 train = True
 
 
@@ -45,7 +45,7 @@ for new_dir in ['models', 'report']:
 
 #input
 x = tf.placeholder(tf.float32, [batch_size,84,84,len(bands_to_use)])
-y = tf.placeholder(tf.float32, [None, n_classes])
+y = tf.placeholder(tf.float32, [batch_size, n_classes])
 
 global_step = tf.Variable(0, trainable=False)
 #learning_rate = tf.train.exponential_decay(start_learning_rate, global_step, decay_steps, decay_base)
@@ -53,7 +53,7 @@ learning_rate = tf.Variable(start_learning_rate)
 
 #net = ExperimentalNet.get_network(x)
 #net = res_net(x)
-net = Resnet.get_network(x, block_config)
+net = Resnet.get_network(x, block_config, global_avg_pool=False, use_2016_update=True)
 
 #cost = tf.reduce_mean(tf.squared_difference(net, y))
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(net, y))
@@ -61,6 +61,9 @@ cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(net, y))
 optimizer = tf.train.MomentumOptimizer(learning_rate, momentum, use_nesterov=True).minimize(cost, global_step=global_step)
 
 rmse = tf.sqrt(tf.reduce_mean(tf.squared_difference(tf.nn.softmax(net),y)))
+
+rmse_acc = tf.squared_difference(tf.nn.softmax(net), y)
+#rmse_fin = tf.sqrt(tf.reduce_mean(val))
 
 init = tf.initialize_all_variables()
 
@@ -87,7 +90,7 @@ with tf.Session() as sess:
                             transform_func=trans_func,
                             band_transform_func=band_trans_func)
                             
-            if epoch_reduce and epoch % epoch_reduce == 0:
+            if epoch_reduce and epoch in epoch_reduce:
                 sess.run(learning_rate.assign(learning_rate.eval() / 10.0))                            
                             
             step = 1
@@ -117,6 +120,10 @@ with tf.Session() as sess:
             print 'Epoch {} finished'.format(epoch)
 
             print 'Testing...'
+            
+            lbls = []
+            preds = []
+            
             # test
             test_step = 1
             test_rmse = 0.0
@@ -125,20 +132,28 @@ with tf.Session() as sess:
                 test_size += batch_size
                 batch_xs, batch_ys= dh.get_next_batch()
 
-                _rmse = sess.run(rmse, feed_dict={x: batch_xs, y: batch_ys})
-                _rmse = pow(_rmse, 2) * batch_size
-                test_rmse += _rmse
+                lbls.append(batch_ys)
+                preds.append(sess.run(net, feed_dict={x: batch_xs}))
+
+
+                _rmse = sess.run(rmse_acc, feed_dict={x: batch_xs, y: batch_ys})
+                test_rmse += _rmse.mean(axis=1).sum()
 
                 test_step += 1
 
             test_rmse = sqrt(test_rmse / float(test_size))
             print colorama.Fore.YELLOW + 'Test RMSE:{}'.format(test_rmse)
 
+            cPickle.dump(lbls, open('lbls_{}'.format(epoch), 'wb'))
+            cPickle.dump(preds, open('preds_{}'.format(epoch), 'wb'))
 
             if save_progress and test_rmse < min_rmse:
                 print 'Saving checkpoint'
                 saver.save(sess, model_dir, global_step=epoch)
                 min_rmse = test_rmse
+                
+                cPickle.dump(lbls, open('../report/model_out/lbls_{}.p'.format(epoch), 'wb'))
+                cPickle.dump(preds, open('../report/model_out/preds_{}.p'.format(epoch), 'wb'))
 
 
             with open(test_progress , mode='a') as f:
@@ -150,15 +165,22 @@ with tf.Session() as sess:
     else:
         ckpt = tf.train.get_checkpoint_state(model_dir)
 
-        dh = DataHelper(batch_size, test_size)
+        dh = DataHelper(batch_size=None)
 
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess, ckpt.model_checkpoint_path)
         else:
             print 'no checkpoint found...'
 
-        batch_xs, _ = dh.get_next_batch()
 
-        predictions = sess.run(net, feed_dict={x: batch_xs})
+        output = ''
+        while dh.testing:
+            print dh._idx
+            
+            x_in, y = dh.get_next_example()
+            y_hat = sess.run(net, feed_dict={x: x_in})
+            
+            output += ','.join(['{0:.9f}'.format(i) for i in np.append(y,y_hat)]) + '\n'
 
-        print predictions
+        with open('../report/prediction_output.csv', 'a') as f:
+            f.write(output)

src/network.py
@@ -223,7 +223,10 @@ class ExperimentalNet:
      
 class Resnet:
     @staticmethod
-    def get_network(x, block_config, is_training=True, use_2016_update=False):
+    def get_network(x, block_config, 
+                    is_training=True, 
+                    global_avg_pool=False,
+                    use_2016_update=False):
         n_classes = 5        
         
         shp =x.get_shape().as_list()
@@ -285,32 +288,51 @@ class Resnet:
                     if first:
                         in_dim *= 2
 
-        # TODO this should be converted to global average pooling        
-        fc_transform = tf.reshape(x, [batch_size, -1])        
-        
-        
-        # fully connected        
-        transformed_dim = fc_transform.get_shape().as_list()[1]
-        fc_weights = {
-            'wf1': Resnet._make_weights([transformed_dim, 2048], 'wf1'),
-            # fully coneected 2048 inputs, 2048 outputs
-            'wf2': Resnet._make_weights([2048, 2048], 'wf2'),
-            # 2048 inputs, 5 outputs (class prediction)
-            'out': Resnet._make_weights([2048, n_classes], 'out')
-        }
-            
-        fc_biases = {
-            'bf1': tf.Variable(tf.constant(0.01, shape=[2048])),
-            'bf2': tf.Variable(tf.constant(0.01, shape=[2048])),
-            'out': tf.Variable(tf.constant(0.1, shape=[n_classes]))
-        }        
 
-        fc1 = Resnet._fc(fc_transform, fc_weights['wf1'], fc_biases['bf1'], tf.nn.relu)
-        fc2 = Resnet._fc(fc1, fc_weights['wf2'], fc_biases['bf2'], tf.nn.relu)
+        output = None
         
-        # output
-        # removed activation function for tf crossentropy loss
-        output = Resnet._fc(fc2, fc_weights['out'], fc_biases['out'], None)
+        # conclude the network with global average pooling
+        if global_avg_pool:
+            with tf.variable_scope('out'):
+                w = Resnet._make_weights([1,1,in_dim,n_classes], 'weights')            
+                x = tf.nn.conv2d(x, w, strides=[1,1,1,1], padding='VALID')
+                
+                shp = x.get_shape().as_list()
+                
+                x = Resnet._avg_pool(x, shp[1])
+                # avg_pool returns the shape [batch_size,1,1,n_clases]
+                # reshape to make it compatible with the label
+                output = tf.reshape(x, [batch_size, 5])
+
+        # conclude the network with fully connected layers
+        else:       
+            fc_transform = tf.reshape(x, [batch_size, -1])        
+            
+            
+            # fully connected        
+            transformed_dim = fc_transform.get_shape().as_list()[1]
+            fc_weights = {
+                'wf1': Resnet._make_weights([transformed_dim, 2048], 'wf1'),
+                # fully coneected 2048 inputs, 2048 outputs
+                'wf2': Resnet._make_weights([2048, 2048], 'wf2'),
+                # 2048 inputs, 5 outputs (class prediction)
+                'out': Resnet._make_weights([2048, n_classes], 'out')
+            }
+                
+            fc_biases = {
+                'bf1': tf.Variable(tf.constant(0.01, shape=[2048])),
+                'bf2': tf.Variable(tf.constant(0.01, shape=[2048])),
+                'out': tf.Variable(tf.constant(0.1, shape=[n_classes]))
+            }        
+    
+            fc1 = Resnet._fc(fc_transform, fc_weights['wf1'], fc_biases['bf1'], tf.nn.relu)
+            fc2 = Resnet._fc(fc1, fc_weights['wf2'], fc_biases['bf2'], tf.nn.relu)
+            
+            # output
+            if is_training:
+                output = Resnet._fc(fc2, fc_weights['out'], fc_biases['out'], None)
+            else:
+                output = Resnet._fc(fc2, fc_weights['out'], fc_biases['out'], tf.nn.softmax)
                 
         return output
         
@@ -452,6 +474,11 @@ class Resnet:
     def _max_pool(img, k, pad='VALID'):
         ks = [1, k, k, 1]
         return tf.nn.max_pool(img, ksize=ks, strides=ks, padding=pad)
+
+    @staticmethod
+    def _avg_pool(x, k):
+        ks = [1, k, k, 1]
+        return tf.nn.avg_pool(x, ksize=ks, strides=ks, padding='VALID')
 
     @staticmethod
     def _fc(x, w, b, act):