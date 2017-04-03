ae937e3500c83f24e401c44303664c993958a73b
ryanhausen <rhausen@ucsc.edu>
2017-04-03 15:09:26 -0700

    Post on adding mock negative points to even out gp fit.

---
+++ docs/make_post.py
@@ -0,0 +1,42 @@
+from __future__ import print_function,division
+
+import datetime
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Post Helper')
+parser.add_argument('title', type=str, help='Post title for display in double-quotes(\")')
+parser.add_argument('--tags', help='Tags for the post')
+parser.add_argument('--date', help='Date for the post in yyyy-mm-dd, optional')
+parser.add_argument('--jekyll', help='Start jekyll server with drafts enabled after post is created.')
+parser.add_argument('--editor', help='The editor command to open post after post has been created')
+
+args = parser.parse_args()
+
+date = datetime.date.today().isoformat()
+tags = ''
+
+hidden_title = args.title.replace(' ', '-').lower()
+display_title = '"' + args.title + '"' 
+
+if args.date:
+    raise NotImplemented()
+
+if args.tags:
+    tags = args.tags
+
+# make img dir
+print('Making img dir..')
+os.mkdir(f'./assets/imgs/{date}')
+
+# make draft
+print(f'Creating draft post {hidden_title}')
+with open(f'./_drafts/{hidden_title}.md', 'w') as f:
+    mathjax = '{% include matjax.html  %}\n'
+    post_meta = '---\nlayout: default\ntitle: {}\ndate: {}\ncategories:{}\n---\n\n'.format(display_title,date,tags)
+    f.write(post_meta + mathjax)
+    
+print('Finished.')
+
+if args.editor:
+    os.system(f'{args.editor} ./_drafts/{hidden_title}.md')
