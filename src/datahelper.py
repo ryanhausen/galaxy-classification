refactored resnet into ResNet. Better documentation and more useful output. Also addressed issue with batch normalization bug.

src/datahelper.py
@@ -16,11 +16,11 @@ from copy import deepcopy
 class DataHelper(object):
     """
     Provides shuffling and batches for the neural net
-    """    
-    
-    def __init__(self, 
-                 batch_size=15, 
-                 train_size=0.8, 
+    """
+
+    def __init__(self,
+                 batch_size=15,
+                 train_size=0.8,
                  shuffle_train=True,
                  augment=True,
                  label_noise=None,
@@ -29,7 +29,7 @@ class DataHelper(object):
                  transform_func=None,
                  band_transform_func=None):
         """
-             label_noise should be the stddev of the gaussian used to generate 
+             label_noise should be the stddev of the gaussian used to generate
              the noise
         """
         self._batch_size = batch_size
@@ -46,43 +46,43 @@ class DataHelper(object):
         self._noise_tbl = pd.read_csv(os.path.join(data_dir, 'noise/noise_range.csv'))
         self._lbl_cols = ['ClSph', 'ClDk', 'ClIr', 'ClPS', 'ClUn']
         self._num_classes = len(self._lbl_cols)
-        self._idx = 0        
-        self.training = True        
-        self.testing = False        
+        self._idx = 0
+        self.training = True
+        self.testing = False
 
         # work in batches
-        if batch_size:        
+        if batch_size:
             num_train_examples = int(len(self._imgs_list) * train_size)
-            
+
             batch_train = num_train_examples % batch_size
             if batch_train != 0:
-                
+
                 if batch_train > batch_size / 2:
                     num_train_examples += batch_train
                 else:
                     num_train_examples -= batch_train
-            
+
                 #msg = 'Batch didnt divide evenly into training examples ' + \
                 #      ' adjusted training size from {} to {}'
                 #print msg.format(train_size, float(num_train_examples) /  float(len(self._imgs_list)))
-            
-            # we want to use the same test images every time so they are set 
+
+            # we want to use the same test images every time so they are set
             # aside before the shuffle
-            self._train_imgs = self._imgs_list[:num_train_examples]        
+            self._train_imgs = self._imgs_list[:num_train_examples]
             self._test_imgs = self._imgs_list[num_train_examples:]
-            
+
             if len(self._train_imgs) % batch_size != 0:
                 err = 'Batch size must divide evenly into training. Batch: {} Train size: {}'
                 raise Exception(err.format(batch_size, len(self._train_imgs)))
-            
+
             if shuffle_train:
                 shuffle(self._train_imgs)
-                
+
         # one example at a time
         else:
             self.testing = True
-        
-    def _augment_image(self, img, img_id):    
+
+    def _augment_image(self, img, img_id):
 
         if self._transform_func:
             img = self._transform_func(img)
@@ -93,20 +93,20 @@ class DataHelper(object):
         y_shift = randint(-4,4)
         flip = randint(0,1)
         scale = math.exp(np.random.uniform(np.log(1.0 / 1.3), np.log(1.3)))
-        brightness = np.random.normal(loc=0.0,scale=0.01)        
-        
+        brightness = np.random.normal(loc=0.0,scale=0.01)
+
         to_origin = DataHelper._translate(42,42)
         flipped = DataHelper._flip(randint(0,1)) if flip else np.identity(3)
         scaled = DataHelper._scale(scale)
         rotated = DataHelper._rotate(rotation)
         shifted = DataHelper._translate(x_shift, y_shift)
-        recenter = DataHelper._translate(-42,-42)                 
+        recenter = DataHelper._translate(-42,-42)
 
         trans = to_origin.dot(flipped).dot(scaled).dot(rotated).dot(shifted).dot(recenter)
         trans = tuple(trans.flatten()[:6])
 
-        bands = ['h','j','v','z']        
-        
+        bands = ['h','j','v','z']
+
         tmp = []
 
         shp_rng = None
@@ -118,19 +118,19 @@ class DataHelper(object):
         for i in range(shp_rng):
             if bands[i] in self._bands:
                 tmp_img = img[:,:,i]
-            
+
                 if self._band_transform_func:
                     tmp_img = self._band_transform_func(tmp_img)
-            
+
                 tmp_img = Image.fromarray(tmp_img)
-                
-                tmp_img = tmp_img.transform((84,84), Image.AFFINE, data=trans, resample=Image.BILINEAR)                
-                
-                tmp_img = np.asarray(tmp_img)            
-                noise = fits.getdata('../data/noise/{}.fits'.format(bands[i]))       
-                
+
+                tmp_img = tmp_img.transform((84,84), Image.AFFINE, data=trans, resample=Image.BILINEAR)
+
+                tmp_img = np.asarray(tmp_img)
+                noise = fits.getdata('../data/noise/{}.fits'.format(bands[i]))
+
                 id_mask = (self._noise_tbl['ID']==img_id) & (self._noise_tbl['band']==bands[i])
-                
+
                 try:
                     rng = tuple(self._noise_tbl.loc[id_mask, ['mn', 'mx']].values[0])
                     noise = self._scale_to(noise, rng, (np.min(noise), np.max(noise)))
@@ -138,180 +138,188 @@ class DataHelper(object):
                     None
                     # log this
                     #print 'unable to rescale noise for {}, {} likely there is no noise rescale from'.format(img_id, bands[i])
-                
+
                 len_noise = None
                 if bands[i] not in ('h','j'):
                     noise = noise.flatten()
                     len_noise = len(noise)-1
-                
-                noise_mask = tmp_img == 0            
-                
-                cpy_img = deepcopy(np.asarray(tmp_img)) 
+
+                noise_mask = tmp_img == 0
+
+                cpy_img = deepcopy(np.asarray(tmp_img))
                 for j in range(cpy_img.shape[0]):
                     for k in range(cpy_img.shape[1]):
                         if noise_mask[j,k]:
-                            if bands[i] in ('h','j'):                        
+                            if bands[i] in ('h','j'):
                                 cpy_img[j,k] = noise[j,k]
                             else:
                                 cpy_img[j,k] = noise[randint(0,len_noise)]
-                
+
                 tmp.append(cpy_img)
-        
+
         if len(tmp) > len(self._bands):
             raise Exception('Image {} didnt augment properly'.format(img_id))
-        
+
         return np.dstack(tmp)
-        
+
         return img
-    
+
     def get_next_batch(self, include_ids=False):
-        if self.training:        
+        if self.training:
             end_idx = self._idx + self._batch_size
             sources = self._train_imgs[self._idx:end_idx]
 
             x = []
             y = []
-            
+
             for s in sources:
-                s_id = s[:-5]                  
-                x_dir = os.path.join(self._imgs_dir,s)  
-                
-                x_tmp =  fits.getdata(x_dir)
-                
+                s_id = s[:-5]
+                x_dir = os.path.join(self._imgs_dir,s)
+
+                try:
+                    x_tmp =  fits.getdata(x_dir)
+                except Exception as e:
+                    print(f'ERROR with {x_dir}')
+                    raise e
+
                 if self._augment:
                     x_tmp = self._augment_image(x_tmp, s_id)
-                                                
+
                 x.append(x_tmp.copy())
                 del x_tmp
 
                 # for the labels we need to prefix GDS_
-                s_id = 'GDS_' + s[:-5]                  
+                s_id = 'GDS_' + s[:-5]
                 lbl = self._lbls.loc[self._lbls['ID']==s_id, self._lbl_cols]
                 lbl = lbl.values.reshape(self._num_classes)
-            
+
                 if self._label_noise:
-                    lbl_noise = np.random.normal(scale=self._label_noise, 
+                    lbl_noise = np.random.normal(scale=self._label_noise,
                                                  size=self._num_classes)
-                                                 
+
                     # add noise and renormalize so we get a proper distribution
                     lbl = DataHelper._rescale_label(lbl + lbl_noise)
-            
-                y.append(lbl)            
-            
+
+                y.append(lbl)
+
             x = np.array(x)
-            y = np.array(y)       
-            
+            y = np.array(y)
+
             if end_idx >= len(self._train_imgs):
                 self.training = False
                 self.testing = True
                 self._idx = 0
             else:
                 self._idx = end_idx
-            
+
             if include_ids:
                 return (sources, x, y)
             else:
                 return (x, y)
-        else:            
-            sources = self._test_imgs[:]            
-            bands = ['h','j','v','z']            
-            
+        else:
+            sources = self._test_imgs[:]
+            bands = ['h','j','v','z']
+
             x = []
             y = []
-            
+
             for s in sources:
-                x_dir = os.path.join(self._imgs_dir,s)          
-                
-                raw = fits.getdata(x_dir)
-                
+                x_dir = os.path.join(self._imgs_dir,s)
+
+                try:
+                    raw =  fits.getdata(x_dir)
+                except Exception as e:
+                    print(f'ERROR with {x_dir}')
+                    raise e
+
                 if self._drop_band:
                     tmp = []
                     for i in range(4):
                         if bands[i] in self._bands:
                             tmp.append(raw[:,:,i])
 
-                    raw = np.dstack(tmp)                            
-                
-                # to avoid os errors copy to a new mem location and 
+                    raw = np.dstack(tmp)
+
+                # to avoid os errors copy to a new mem location and
                 # remove the file handle
-                x.append(deepcopy(raw))                                
-                del raw                
-                
-                s_id = 'GDS_' + s[:-5]                
+                x.append(deepcopy(raw))
+                del raw
+
+                s_id = 'GDS_' + s[:-5]
                 lbl = self._lbls.loc[self._lbls['ID']==s_id, self._lbl_cols]
                 y.append(lbl.values.reshape(self._num_classes))
-            
+
             x = np.array(x)
-            y = np.array(y)       
-            
+            y = np.array(y)
+
             if include_ids:
                 return (sources, x, y)
             else:
                 return (x, y)
-            
+
     def get_class_distribution(self):
         cls_count_train = np.zeros([5,])
         cls_count_test = np.zeros([5,])
-        
-      
+
+
         for i in range(len(self._train_imgs)):
             src = self._train_imgs[i]
             s_id = 'GDS_' + src[:-5]
             lbl = self._lbls.loc[self._lbls['ID']==s_id, self._lbl_cols]
             lbl = lbl.values.reshape(self._num_classes)
-            
+
             cls_count_train[np.argmax(lbl)] += 1
-            
+
         for i in range(len(self._test_imgs)):
             src = self._test_imgs[i]
             s_id = 'GDS_' + src[:-5]
             lbl = self._lbls.loc[self._lbls['ID']==s_id, self._lbl_cols]
             lbl = lbl.values.reshape(self._num_classes)
-            
+
             cls_count_test[np.argmax(lbl)] += 1
-            
+
         return (cls_count_train, cls_count_test)
 
     def get_next_example(self, img_id=None):
         bands = ['h','j','v','z']
-    
+
         source = None
         if img_id:
             source = img_id + '.fits'
             self.testing = False
-        else:        
+        else:
             source = self._imgs_list[self._idx]
             self._idx += 1
-        
+
         if self._idx >= len(self._imgs_list):
             self.testing = False
-        
+
         x = fits.getdata(os.path.join(self._imgs_dir, source))
-        
+
         tmp_x = []
         for i in range(4):
             if bands[i] in self._bands:
                 tmp_x.append(x[:,:,i])
-        
+
         x = np.dstack(tmp_x).reshape(1,84,84,len(self._bands))
-        
-        
+
+
         y = self._lbls.loc[self._lbls['ID']=='GDS_'+ source[:-5], self._lbl_cols]
         y = y.values.reshape(self._num_classes)
-        
+
         return (x,y)
 
-       
-    # Helper 
+
+    # Helper
     @staticmethod
     def _rescale_label(y):
-        return y / y.sum()            
-            
+        return y / y.sum()
+
     # http://stackoverflow.com/a/5295202
-    def _scale_to(self, x, rng, minmax):       
+    def _scale_to(self, x, rng, minmax):
         a, b = rng
         mn, mx = minmax
-    
+
         return (((b-a)*(x-mn))/(mx-mn)) + a
 
     #http://stackoverflow.com/questions/34968722/softmax-function-python
@@ -319,20 +327,20 @@ class DataHelper(object):
     def _softmax(x):
         exp_x = np.exp(x - np.max(x))
         return exp_x / exp_x.sum()
-      
-    @staticmethod        
+
+    @staticmethod
     def _translate(u,v):
         return np.array([
                 [1.0, 0.0, float(u)],
                 [0.0, 1.0, float(v)],
                 [0.0, 0.0, 1.0]
             ])
-    @staticmethod 
+    @staticmethod
     def _rotate(angle):
-        angle = -math.radians(angle)                
+        angle = -math.radians(angle)
         cos = math.cos(angle)
         sin = math.sin(angle)
-        
+
         return np.array([
                 [cos, sin, 0.0],
                 [-sin, cos, 0.0],

src/evaluate.py
@@ -17,7 +17,7 @@ def _rmse(yh, ys):
     return tf.sqrt(tf.reduce_mean(tf.squared_difference(yh,ys)))
 
 def _cross_entropy(yh, ys):
-    return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(yh, ys))
+    return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=yh, labels=ys))
 
 def _class_accuracy_part1(yh,ys):
     return ys, tf.to_float(tf.nn.in_top_k(yh,ys,1))

src/main.py
@@ -18,8 +18,9 @@ y = None
 logger = None
 
 def main(config=None):
-    try:
 
+
+    try:
         global params
         global x
         global y
@@ -33,6 +34,8 @@ def main(config=None):
                 params = _type_convert(json.load(f))
 
         # add some code to validate dictionary
+        if params['print']:
+            tf.logging.set_verbosity(tf.logging.INFO)
 
         channels = len(params['bands'])
 
@@ -45,22 +48,22 @@ def main(config=None):
 #                                 params['global_avg_pool'],
 #                                 params['2016_update'])
 
-        net = ResNet.build_graph(x, params['block_config'], params['train'],)
-
+        net = ResNet.build_graph(x, params['block_config'], params['train'])
+        eval_net = ResNet.build_graph(x, params['block_config'], False)
 
         if params['train']:
-            _train_network(net)
+            _train_network(net, eval_net)
         else:
-            _use_network(net)
+            _use_network(eval_net)
 
     except KeyboardInterrupt:
         print('\nInterrupted')
-        run = 'y' == raw_input('Run eval statistics?(y/[n])')
+        run = 'y' == input('Run eval statistics?(y/[n])')
         if run:
             print('Not implemented yet')
 
 
-def _train_network(net):
+def _train_network(net, eval_net):
     global params
     global x
     global y
@@ -75,15 +78,17 @@ def _train_network(net):
     else:
         learning_rate = tf.Variable(params['start_learning_rate'])
 
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(net, y))
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=net, labels=y))
 
     # find a way tp paramertize the optimizer
-    optimizer = tf.train.MomentumOptimizer(learning_rate,
-                                           params['momentum'],
-                                           params['nesterov'])
+#    optimizer = tf.train.MomentumOptimizer(learning_rate,
+#                                           params['momentum'],
+#                                           params['nesterov'])
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+
     optimize = optimizer.minimize(cost, global_step=iters)
 
-    init = tf.initialize_all_variables()
+    init = tf.global_variables_initializer()
     saver = tf.train.Saver()
 
     learning_rate_reduce = params['learning_rate_reduce']
@@ -97,7 +102,7 @@ def _train_network(net):
 
         for epoch in range(1, params['epoch_limit']+1):
             if params['print']:
-                print(epoch)
+                tf.logging.info(f'Epoch:{epoch}')
 
             dh = DataHelper(batch_size=params['batch_size'],
                         train_size=params['train_size'],
@@ -106,37 +111,44 @@ def _train_network(net):
                         transform_func=eval(params['trans_func']) if params['trans_func'] else None)
 
             if learning_rate_reduce and epoch in learning_rate_reduce:
-                sess.run(learning_rate.assign(learning_rate.eval() / 10.0))
+                sess.run(learning_rate.assign(learning_rate.eval() / 10))
+
+            if params['print']:
+                tf.logging.info('Training...')
 
             while dh.training:
                 batch_xs, batch_ys = dh.get_next_batch()
                 sess.run(optimize, feed_dict={x:batch_xs, y:batch_ys})
 
                 if iters.eval() % 20 == 0:
-                    evaluate.evaluate(sess, net, x, y, batch_xs, batch_ys, params['train_progress'])
+                    if params['print']:
+                        tf.logging.info('Evaluating...')
 
-            #testing
+                    evaluate.evaluate(sess, eval_net, x, y, batch_xs, batch_ys, params['train_progress'])
+
+                    if params['print']:
+                        tf.logging.info('Training...')
+
+            if params['print']:
+                tf.logging.info('Testing...')
+
             srcs, batch_xs, batch_ys = dh.get_next_batch(include_ids=True)
-            results = evaluate.evaluate(sess, net, x, y, batch_xs, batch_ys, params['test_progress'])
+            results = evaluate.evaluate(sess, eval_net, x, y, batch_xs, batch_ys, params['test_progress'])
 
             if params['save_progress'] and results[0] > top_result:
                 if params['print']:
-                    print('Saving checkpoint')
+                    tf.logging.info('Saving checkpoint')
                 saver.save(sess, params['model_dir'], global_step=iters)
                 top_result = results[0]
 
                 cPickle.dump(srcs, open('../report/model_out/srcs_{}.p'.format(epoch), 'wb'))
                 cPickle.dump(batch_ys, open('../report/model_out/lbls_{}.p'.format(epoch), 'wb'))
-                cPickle.dump(sess.run(tf.nn.softmax(net), feed_dict={x: batch_xs}), open('../report/model_out/preds_{}.p'.format(epoch), 'wb'))
+                cPickle.dump(sess.run(eval_net, feed_dict={x: batch_xs}), open('../report/model_out/preds_{}.p'.format(epoch), 'wb'))
 
+            if params['print']:
+                tf.logging.info('Epoch took {} seconds'.format(time.time() - start))
 
-
-
-
-
-    if params['print']:
-        print('Epoch took {} seconds'.format(time.time() - start))
-
+    # This needs to be printed so that the async trainer can see the result
     if params['rtrn_eval']:
         print(top_result)
 

src/network.py
@@ -1,6 +1,6 @@
 import math
 import tensorflow as tf
-from tensorflow.contrib.layers.python.layers import batch_norm as batch_norm
+from tensorflow.contrib.layers import batch_norm
 
 class SimpleNet:
     seed = None
@@ -286,8 +286,6 @@ class ExperimentalNet:
             # k used to be 2
             conv1 = max_pool(conv1, k=4)
 
-            #print 'conv1: {}'.format(conv1.get_shape())
-
             # Second Covolution layer
             conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
             conv2 = max_pool(conv2, k=2)
@@ -343,18 +341,37 @@ class ResNet:
     X = tf.placeholder(tf.float32, [None, 84, 84, 4])
     Y = tf.placeholder(tf.float32, [None, 5])
 
+    #https://stackoverflow.com/a/38161314
+    @staticmethod
+    def print_total_params():
+        """
+            outputs the number of learnable parameters
+        """
+
+        total_parameters = 0
+        for variable in tf.trainable_variables():
+            # shape is an array of tf.Dimension
+            shape = variable.get_shape()
+            variable_parametes = 1
+            for dim in shape:
+                variable_parametes *= dim.value
+            total_parameters += variable_parametes
+        tf.logging.info(f'TOTAL NUMBER OF PARAMTERS:{total_parameters}')
+
     @staticmethod
     def build_graph(x, block_config, is_training):
         """
             x: a tf.placeholder
             block_config: list of ints that determines the number of residual
                           blocks in each block_segment
+            is_training: indicates training vs inference
         """
         # if the network is large use the bottleneck architecture
         is_large = sum(block_config) * 2 >= 50
+        reuse = None if is_training else True
 
         # this initial convolution reduces our image WxH by half
-        with tf.variable_scope('conv1'):
+        with tf.variable_scope('conv1', reuse=reuse):
             x = ResNet.conv1(x)
 
         # begin residual blocks
@@ -362,25 +379,33 @@ class ResNet:
             num_blocks = block_config[segment_idx]
 
             for block_idx in range(num_blocks):
-                with tf.variable_scope(f'seg{segment_idx}block{block_idx}'):
+                tf.logging.info(f'SEG{segment_idx}-BLOCK{block_idx}')
+                with tf.variable_scope(f'seg{segment_idx}block{block_idx}', reuse=reuse):
                     # if this is the first block the WxH are reduced and  the number
                     # of filters is increased
                     reduce = block_idx == 0
+                    first = segment_idx==0
                     if is_large:
-                        x = ResNet.bottleneck(x, reduce, segment_idx==0)
+                        x = ResNet.bottleneck(x, reduce, first, is_training)
                     else:
-                        x = ResNet.block(x, reduce)
+                        x = ResNet.block(x, reduce, first, is_training)
 
-        with tf.variable_scope('out'):
+        with tf.variable_scope('out', reuse=reuse):
             x = ResNet.global_average_pooling(x)
+
+        ResNet.print_total_params()
 
         if is_training:
             return x
         else:
             return tf.nn.softmax(x)
 
+    # https://arxiv.org/pdf/1312.4400.pdf
     @staticmethod
     def global_average_pooling(x):
+        tf.logging.info('GLOBAL-AVG-POOLING--------------')
+        tf.logging.info(f'X-IN:{x.get_shape().as_list()}')
+
         in_channels = x.get_shape().as_list()[-1]
 
         k = [1, 1, in_channels, 5]
@@ -389,14 +414,26 @@ class ResNet:
         x = tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding='VALID')
         x = tf.reduce_mean(x, [1, 2])
 
+        tf.logging.info(f'X-OUT:{x.get_shape().as_list()}')
+        tf.logging.info('--------------------------------')
         return tf.squeeze(x)
 
     @staticmethod
-    def bottleneck(x, reduce, first_block):
+    def bottleneck(x, reduce, first_block, is_training):
+        """
+            implements a bottleneck residual block which consists of a
+            1x1 convolution -> 3x3 convolution -> 1x1 convolution -> shortcut
+        """
+        reuse = None if is_training else True
         in_channels = x.get_shape().as_list()[-1]
-        print(x.get_shape().as_list())
+
+        shp = lambda t: t.get_shape().as_list()
+        tf.logging.info('BOTTLENECK:---------------------')
+        tf.logging.info(f'X-IN:{shp(x)}')
 
         if reduce:
+            tf.logging.info('Reducing Image Size')
+
             if first_block:
                 bottleneck_channels = in_channels
                 stride = 1
@@ -408,39 +445,66 @@ class ResNet:
 
             out_channels = in_channels * 2
         else:
-            bottleneck_channels = in_channels // 4
+            if first_block:
+                bottleneck_channels = in_channels // 2
+            else:
+                bottleneck_channels = in_channels // 4
+
             out_channels = in_channels
             pad = 'SAME'
             stride = 1
 
         # 1x1 conv1
-        with tf.variable_scope('op1'):
+        with tf.variable_scope('op1', reuse=reuse):
             k = [1, 1, in_channels, bottleneck_channels]
-            f_x = ResNet.block_op(x, k, 1, 'VALID')
+            f_x = ResNet.block_op(x, k, 1, 'VALID', is_training)
+
+        tf.logging.debug(f'H(X):{shp(f_x)}')
 
         # 3x3 conv2
-        with tf.variable_scope('op2'):
+        with tf.variable_scope('op2', reuse=reuse):
+            if reduce and first_block == False:
+                f_x = tf.pad(f_x, [[0, 0], [1, 1], [1, 1], [0, 0]])
+
             k = [3, 3, bottleneck_channels, bottleneck_channels]
-            f_x = ResNet.block_op(f_x, k, stride, pad)
+            f_x = ResNet.block_op(f_x, k, stride, pad, is_training)
+
+        tf.logging.debug(f'H(X):{shp(f_x)}')
+
 
         # 1x1 conv3
-        with tf.variable_scope('op3'):
+        with tf.variable_scope('op3', reuse=reuse):
             k = [1, 1, bottleneck_channels, out_channels]
-            f_x = ResNet.block_op(f_x, k, 1, 'VALID')
+            f_x = ResNet.block_op(f_x, k, 1, 'VALID', is_training)
+
+        tf.logging.debug(f'H(X):{shp(f_x)}')
 
         if reduce:
             k = [1, 1, in_channels, out_channels]
             w = tf.get_variable('proj', shape=k, initializer=ResNet.var_init)
             x = tf.nn.conv2d(x, w, [1, stride, stride, 1], padding='VALID')
 
+
+        tf.logging.debug(f'X+H(X):{shp(f_x)} + {shp(x)}')
+        tf.logging.info(f'X-OUT:{shp(x)}')
+        tf.logging.info('--------------------------------')
         return tf.add(x, f_x)
 
-
     @staticmethod
-    def block(x, reduce):
+    def block(x, reduce, first_block, is_training):
+        """
+            implements a standard residual block which consists of a
+            3x3 convolution -> 3x3 convolution -> shortcut
+        """
         in_channels = x.get_shape().as_list()[-1]
+        reuse = None if is_training else True
 
-        if reduce:
+        shp = lambda t: t.get_shape().as_list()
+        tf.logging.info('STANDARD:-----------------------')
+        tf.logging.info(f'X-IN:{shp(x)}')
+
+        if reduce and first_block==False:
+            tf.logging.info('Reducing Image Size')
             out_channels = in_channels * 2
             pad = 'VALID'
             stride = 2
@@ -450,45 +514,68 @@ class ResNet:
             stride = 1
 
         # 3x3 conv1
-        with tf.variable_scope('op1'):
-            k = [3, 3, in_channels, out_channels]
-            f_x = ResNet.block_op(x, k, stride, pad)
+        with tf.variable_scope('op1', reuse=reuse):
+            if reduce and first_block == False:
+                f_x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]])
+                k = [3, 3, in_channels, out_channels]
+                f_x = ResNet.block_op(f_x, k, stride, pad, is_training)
+            else:
+                k = [3, 3, in_channels, out_channels]
+                f_x = ResNet.block_op(x, k, stride, pad, is_training)
+
+        tf.logging.debug(f'H(X):{shp(f_x)}')
 
         # 3x3 conv2
-        with tf.variable_scope('op2'):
+        with tf.variable_scope('op2', reuse=reuse):
             k = [3, 3, out_channels, out_channels]
-            f_x = ResNet.block_op(f_x, k, 1, 'SAME')
+            f_x = ResNet.block_op(f_x, k, 1, 'SAME', is_training)
+
+        tf.logging.debug(f'H(X):{shp(f_x)}')
 
         # project x into the correct dimensions if there was a reduction
-        if reduce:
+        if reduce and first_block==False:
             k = [1, 1, in_channels, out_channels]
             w = tf.get_variable('proj', k, initializer=ResNet.var_init)
-            x = tf.nn.conv2d(x, w, stride=[1, stride, stride, 1], padding=pad)
+            x = tf.nn.conv2d(x, w, [1, stride, stride, 1], padding=pad)
 
-        return x + f_x
+        tf.logging.debug(f'X+H(X):{shp(f_x)} + {shp(x)}')
+        tf.logging.info(f'X-OUT:{shp(x)}')
+        tf.logging.info('--------------------------------')
+        return tf.add(x, f_x)
 
     @staticmethod
-    def block_op(x, k, s, pad):
-        w = tf.get_variable('w', shape=k, initializer=ResNet.var_init)
-        b = tf.get_variable('b', shape=k[-1:], initializer=ResNet.var_init)
-
+    def block_op(x, k, s, pad, is_training):
+        """
+            implements the set of operations that accompany each
+            convolution. batchnorm -> relu -> conv2d
+        """
+        x = batch_norm(x, is_training=is_training, decay=0.9, updates_collections=None)
         x = tf.nn.relu(x)
+
+        w = tf.get_variable('w', shape=k, initializer=ResNet.var_init)
         x = tf.nn.conv2d(x, w, strides=[1, s, s, 1], padding=pad)
-        x = tf.nn.bias_add(x, b)
 
         return x
 
     @staticmethod
     def conv1(x):
+        tf.logging.info('INITAL-CONV---------------------')
+        tf.logging.info(f'X-IN:{x.get_shape().as_list()}')
         #              Batch   Width   Height  Channels
         x = tf.pad(x, [[0, 0], [3, 3], [3, 3], [0, 0]], 'CONSTANT')
 
         w = tf.get_variable('w', shape=[7, 7, 4, 64], initializer=ResNet.var_init)
         s = [1, 2, 2, 1]
-        return tf.nn.relu(tf.nn.conv2d(x, w, s, 'VALID'))
 
-    @staticmethod
+        x = tf.nn.relu(tf.nn.conv2d(x, w, s, 'VALID'))
+
+        tf.logging.info(f'X-OUT:{x.get_shape().as_list()}')
+        tf.logging.info('--------------------------------')
+        return x
+
+
     #https://arxiv.org/abs/1502.01852
+    @staticmethod
     def var_init(shape, dtype, partition_info=None):
         std = None
 