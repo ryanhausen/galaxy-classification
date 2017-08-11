Working CNN RNN. Fixed problem with gradient. Added tensorboard hookds for gradient

src/datahelper.py
@@ -231,7 +231,11 @@ class DataHelper(object):
 
         return img_names
 
-    def get_next_batch(self, include_ids=False, iter_based=False, force_test=False):
+    def get_next_batch(self,
+                       include_ids=False,
+                       iter_based=False,
+                       force_test=False,
+                       split_channels=False):
         if self.training and force_test==False:
             x, y = [], []
 
@@ -263,13 +267,22 @@ class DataHelper(object):
                 if self._augment:
                     x_tmp = self._augment_image(x_tmp, s_id)
 
-                x.append(x_tmp.copy())
+                if split_channels:
+                    x.extend([x_tmp[:,:,i].reshape([84,84,1]) for i in range(x_tmp.shape[2])])
+                else:
+                    x.append(x_tmp.copy())
+
                 del x_tmp
 
                 # for the labels we need to prefix GDS_
                 s_id = 'GDS_' + s[:-5]
                 lbl = self._lbls.loc[self._lbls['ID']==s_id, self._lbl_cols]
                 lbl = lbl.values.reshape(self._num_classes)
+
+
+#                _tmp = np.zeros_like(lbl)
+#                _tmp[lbl.argmax()] = 1.0
+#                lbl = _tmp
 
                 if self._label_noise:
                     lbl_noise = np.random.normal(scale=self._label_noise,
@@ -311,9 +324,12 @@ class DataHelper(object):
 
                     raw = np.dstack(tmp)
 
-                # to avoid os errors copy to a new mem location and
-                # remove the file handle
-                x.append(deepcopy(raw))
+
+                if split_channels:
+                    x.extend([raw[:,:,i].reshape([84,84,1]) for i in range(raw.shape[2])])
+                else:
+                    x.append(raw.copy())
+
                 del raw
 
                 s_id = 'GDS_' + s[:-5]

src/network.py
@@ -4,61 +4,13 @@ import tensorflow as tf
 from tensorflow.contrib.layers import batch_norm
 from tensorflow.contrib.rnn import BasicRNNCell, static_rnn, BasicLSTMCell
 
-class SimpleRNN:
-    num_channels = 4
-    num_hidden = 512
-    out_classes = 5
-    x = tf.placeholder(tf.float32, [None,num_channels,out_classes])
-    y = tf.placeholder(tf.float32, [None,out_classes])
-
-    @staticmethod
-    #https://arxiv.org/abs/1502.01852
-    def var_init(shape, dtype, partition_info=None):
-        std = None
-
-        # bias
-        if len(shape) ==1:
-            return tf.constant(0.01, shape=shape)
-        # fc Layer
-        if len(shape) == 2:
-            std = math.sqrt(1/shape[0])
-        # conv layer
-        elif len(shape) == 4:
-            k_sqr_d = shape[0] * shape[1] * shape[2]
-            std = math.sqrt(2 / k_sqr_d)
-
-        return tf.truncated_normal(shape, stddev=std, seed=SimpleNet.seed)
-
-
-    def build_graph(x) :
-
-       # reformat for the RNN
-       x = tf.unstack(x, SimpleRNN.num_channels, 1)
-
-       # rnn
-       with tf.variable_scope('rnn'):
-           rnn = BasicLSTMCell(SimpleRNN.num_hidden)
-
-       # process rnn
-       outputs, states = static_rnn(rnn, x, dtype=tf.float32)
-
-
-       with tf.variable_scope('rnn_act'):
-           w = tf.get_variable('w', shape=[SimpleRNN.num_hidden, SimpleRNN.out_classes], initializer=SimpleRNN.var_init)
-           b = tf.get_variable('b', shape=[SimpleRNN.out_classes], initializer=SimpleRNN.var_init)
-
-       return tf.nn.bias_add(tf.matmul(outputs[-1], w), b)
-
-
-
-
 
 class SimpleNet:
     num_channels = 4
 
     seed = None
-    x = tf.placeholder(tf.float32, [None,84,84,4])
-    y = tf.placeholder(tf.float32, [None, 5])
+    x = tf.placeholder(tf.float32, [None,84,84,1])
+    y = tf.placeholder(tf.float32, [None, 100])
 
 
     @staticmethod
@@ -97,11 +49,11 @@ class SimpleNet:
         return tf.truncated_normal(shape, stddev=std, seed=SimpleNet.seed)
 
     @staticmethod
-    def conv2d(x, k, pad='SAME'):
+    def conv2d(x, k, s=1, pad='SAME'):
         w = tf.get_variable('w', shape=k, initializer=SimpleNet.var_init)
         b = tf.get_variable('b', shape=[k[-1]], initializer=SimpleNet.var_init)
 
-        strides = [1,1,1,1]
+        strides = [1,s,s,1]
 
         return tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(x, w, strides, pad), b))
 
@@ -124,11 +76,11 @@ class SimpleNet:
 
     @staticmethod
     def c1w_shape():
-        return [3,3,1,32]
+        return [5,5,1,32]
 
     @staticmethod
     def c2w_shape():
-        return [5, 5, 32, 64]
+        return [3,3,32,64]
 
     @staticmethod
     def c3w_shape():
@@ -151,41 +103,33 @@ class SimpleNet:
         return [2048, 5]
 
     @staticmethod
-    def build_cnn(x, reuse, global_avg_pooling=False):
+    def build_cnn(x, reuse, global_avg_pooling=False, raw_out=False):
 
         tf.logging.info(f'IN-SHAPE:{x.shape.as_list()}')
 
         with tf.variable_scope('conv1', reuse=reuse):
-            x = SimpleNet.conv2d(x, SimpleNet.c1w_shape())
+            x = SimpleNet.conv2d(x, SimpleNet.c1w_shape(), s=2, pad='VALID')
+            tf.logging.info(f'CONV1:{x.shape.as_list()}')
 
-        tf.logging.info(f'CONV1:{x.shape.as_list()}')
+            #x = SimpleNet.max_pool(x)
 
-        x = SimpleNet.max_pool(x)
-
-        tf.logging.info(f'MAXPOOL:{x.shape.as_list()}')
+#            tf.logging.info(f'MAXPOOL:{x.shape.as_list()}')
 
         with tf.variable_scope('conv2', reuse=reuse):
-            x = SimpleNet.conv2d(x, SimpleNet.c2w_shape())
+            x = SimpleNet.conv2d(x, SimpleNet.c2w_shape(), s=2, pad='VALID')
+            tf.logging.info(f'CONV2:{x.shape.as_list()}')
 
-        tf.logging.info(f'CONV2:{x.shape.as_list()}')
+            #x = SimpleNet.max_pool(x)
 
-        x = SimpleNet.max_pool(x)
-
-        tf.logging.info(f'MAXPOOL:{x.shape.as_list()}')
+            #tf.logging.info(f'MAXPOOL:{x.shape.as_list()}')
 
         with tf.variable_scope('conv3', reuse=reuse):
-            x = SimpleNet.conv2d(x, SimpleNet.c3w_shape())
-
-        tf.logging.info(f'CONV3:{x.shape.as_list()}')
+            x = SimpleNet.conv2d(x, SimpleNet.c3w_shape(), pad='VALID')
+            tf.logging.info(f'CONV3:{x.shape.as_list()}')
 
         with tf.variable_scope('conv4', reuse=reuse):
-            x = SimpleNet.conv2d(x, SimpleNet.c4w_shape())
-
-        tf.logging.info(f'CONV4:{x.shape.as_list()}')
-
-        x = SimpleNet.max_pool(x)
-
-        tf.logging.info(f'MAXPOOL:{x.shape.as_list()}')
+            x = SimpleNet.conv2d(x, SimpleNet.c4w_shape(), pad='VALID')
+            tf.logging.info(f'CONV4:{x.shape.as_list()}')
 
         if global_avg_pooling:
             dim_in = x.shape.as_list()[-1]
@@ -203,10 +147,15 @@ class SimpleNet:
             x = tf.reshape(x, [-1, 5])
 
             tf.logging.info(f'GBL_AVG_POOL:{x.shape.as_list()}')
+        elif raw_out:
+            x_flat = np.prod(x.get_shape().as_list()[1:])
+            x = tf.reshape(x, [-1, x_flat])
+
+            tf.logging.info(f'RAWOUT:{x.shape.as_list()}')
+
 
         else:
             x_flat = np.prod(x.get_shape().as_list()[1:])
-
             x = tf.reshape(x, [-1, x_flat])
 
             tf.logging.info(f'RESHAPE:{x.shape.as_list()}')
@@ -230,6 +179,8 @@ class SimpleNet:
 
             tf.logging.info(f'FC3:{x.shape.as_list()}')
 
+        SimpleNet.y = x.shape
+
         return x
 
     @staticmethod
@@ -242,10 +193,58 @@ class SimpleNet:
 
         ys = []
         for channel in range(SimpleNet.num_channels):
-            ys.append(SimpleNet.build_cnn(x[channel], reuse=channel>0, global_avg_pooling=True))
+            ys.append(SimpleNet.build_cnn(x[channel], reuse=channel>0, raw_out=True))
 
         return tf.stack(ys, axis=1)
 
+class SimpleRNN:
+    seed = None
+
+    num_channels = 4
+    num_hidden = 5
+    out_classes = 5
+    x = tf.placeholder(tf.float32, [None,num_channels,SimpleNet.y.shape.as_list()[-1]])
+    y = tf.placeholder(tf.float32, [None,out_classes])
+
+    @staticmethod
+    #https://arxiv.org/abs/1502.01852
+    def var_init(shape, dtype, partition_info=None):
+        std = None
+
+        # bias
+        if len(shape) ==1:
+            return tf.constant(0.01, shape=shape)
+        # fc Layer
+        if len(shape) == 2:
+            std = math.sqrt(1/shape[0])
+        # conv layer
+        elif len(shape) == 4:
+            k_sqr_d = shape[0] * shape[1] * shape[2]
+            std = math.sqrt(2 / k_sqr_d)
+
+        return tf.truncated_normal(shape, stddev=std, seed=SimpleRNN.seed)
+
+
+    def build_graph(x) :
+
+       # reformat for the RNN
+       x = tf.reshape(x, [-1, 4, np.prod(x.shape.as_list()[1:])])
+       x = tf.unstack(x, num=4, axis=1)
+
+       # rnn
+       with tf.variable_scope('rnn'):
+           rnn = BasicLSTMCell(SimpleRNN.num_hidden)
+
+       # process rnn
+       outputs, states = static_rnn(rnn, x, dtype=tf.float32)
+
+#       with tf.variable_scope('rnn_act'):
+#           w = tf.get_variable('w', shape=[SimpleRNN.num_hidden, SimpleRNN.out_classes], initializer=SimpleRNN.var_init)
+#           b = tf.get_variable('b', shape=[SimpleRNN.out_classes], initializer=SimpleRNN.var_init)
+#
+#       return tf.nn.bias_add(tf.matmul(outputs[-1], w), b)
+
+       return outputs[-1]
 
 
 

src/rnn_experiment.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 # modeled after https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/recurrent_network.py
 
-import time
+from colorama import init, Fore
+init(autoreset=True)
+red = lambda s: Fore.RED + s
+yellow = lambda s: Fore.YELLOW + s
+green = lambda s: Fore.GREEN + s
 
 import numpy as np
 import tensorflow as tf
@@ -13,42 +17,76 @@ from datahelper import DataHelper
 tf.logging.set_verbosity('INFO')
 
 iters = tf.Variable(1, trainable=False)
-learning_rate = 0.1
+learning_rate = 1e-6
+iter_limit = 750
+batch_size = 50
+seed = None
+
 
 x = SimpleNet.x
-cnn = SimpleNet.build_graph(x)
+
+cnn = SimpleNet.build_cnn(x, reuse=None, raw_out=True)
 
 rnn = SimpleRNN.build_graph(cnn)
 y = SimpleRNN.y
 
 infer = tf.nn.softmax(rnn)
 
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=rnn, labels=y))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss, global_step=iters)
+loss = 100*tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=rnn, labels=y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+
+grads = optimizer.compute_gradients(loss)
+#non_nan = [(tf.where(tf.is_nan(grad), tf.zeros_like(grad), grad), var) for grad, var in grads]
+clipped = [(tf.clip_by_value(grad, -0.5, 0.5), var) for grad, var in grads]
+update = optimizer.apply_gradients(clipped, global_step=iters)
+
+with tf.name_scope('summaries'):
+    for grad, var in clipped:
+        tf.summary.histogram(f'Gradients/{var.name}', grad)
 
 init = tf.global_variables_initializer()
 
 #https://stackoverflow.com/a/44983523
 norm = lambda a: 2*(a - np.max(a))/-np.ptp(a)-1
 
-dh = DataHelper(batch_size=50, band_transform_func=norm)
+dh = DataHelper(batch_size=batch_size)
+epoch = 1
+len_epoch = len(dh._train_imgs)
+print(f'Epoch length = {len_epoch}')
 
-iter_limit = 1000
+summaries = tf.summary.merge_all()
+saver = tf.train.Saver()
+
 with tf.Session() as sess:
     sess.run(init)
+    summaryWriter = tf.summary.FileWriter('./tf-log', graph=sess.graph)
 
-    print(SimpleNet.print_total_params())
+    SimpleNet.print_total_params()
 
     while iters.eval() <= iter_limit:
         tf.logging.info(f'Iter:{iters.eval()}...')
-        batch_xs, batch_ys = dh.get_next_batch(iter_based=True)
-        sess.run(optimizer, feed_dict={x:batch_xs, y:batch_ys})
+        batch_xs, batch_ys = dh.get_next_batch(iter_based=True, split_channels=True)
+        sess.run(update, feed_dict={x:batch_xs, y:batch_ys})
 
-        if iters.eval() % 10 == 0:
+
+
+
+        current_iter = iters.eval()
+        if current_iter % 10 == 0:
             evals = evaluate.evaluate(sess, infer, x, y, batch_xs, batch_ys, '../report/train_progress.csv')
+            s = sess.run(summaries, feed_dict={x:batch_xs, y:batch_ys})
+            summaryWriter.add_summary(s, current_iter)
 
-        if iters.eval() % 50 == 0:
-            tf.logging.info('Testing...')
-            batch_xs, batch_ys = dh.get_next_batch(iter_based=True, force_test=True)
+        if current_iter % 50 == 0:
+            tf.logging.info(yellow('Testing...'))
+            batch_xs, batch_ys = dh.get_next_batch(iter_based=True, force_test=True, split_channels=True)
             evals = evaluate.evaluate(sess, infer, x, y, batch_xs, batch_ys, '../report/test_progress.csv')
 
+
+            save_path = saver.save(sess, f'../models/cnn-rnn-{current_iter}.ckpt')
+            tf.logging.info(green(f'Checkpoint: {save_path}'))
+
+
+        if (current_iter * batch_size) > (len_epoch * epoch):
+            tf.logging.info(red(f'Epoch {epoch} complete.'))
+            epoch += 1