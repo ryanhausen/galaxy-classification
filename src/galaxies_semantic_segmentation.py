41e2d81440efa58fa7a5d83bf9c17cfda4eded35
Ryan Hausen <ryan.hausen@gmail.com>
2018-03-08 00:22:28 -0800

    Dataset Pipline working
    Need to filter odd sized segmaps, and filter unkown sources

--- src/galaxies_semantic_segmentation.py
+++ src/galaxies_semantic_segmentation.py
@@ -18,33 +18,40 @@ class Dataset:
         all_imgs = os.listdir(img_dir)
         split_idx = int(len(all_imgs) * split)
 
-        train_x = all_imgs[:split_idx]
-        test_x = all_imgs[split_idx:]
+        with_path = lambda i: os.path.join(img_dir, i)
+        train_x = np.array([with_path(img) for img in all_imgs[:split_idx]])
+        test_x = np.array([with_path(img) for img in all_imgs[split_idx:]])
 
-        labels = pd.read_csv(labels_dir)
+        train_y, train_segmap = zip(*Dataset._get_img_labels(labels_dir, train_x))
+        train_y, train_segmap = np.array(train_y), np.array(train_segmap)
 
-        train_y = Dataset._get_img_labels(labels, train_x)
-        test_y = Dataset._get_img_labels(labels, test_x)
+        test_y, test_segmap = zip(*Dataset._get_img_labels(labels_dir, test_x))
+        test_y, test_segmap = np.array(test_y), np.array(test_segmap)
+
+        #print(train_x.shape, train_y.shape)
 
         self.batch_size = batch_size
-        self.train_data = tf.data.Dataset.from_tensor_slices((train_x, train_y))
-        self.test_data = tf.data.Dataset.from_tensor_slices((test_x, test_y))
+        self.train_data = tf.data.Dataset.from_tensor_slices((train_x, train_y, train_segmap))
+        self.test_data = tf.data.Dataset.from_tensor_slices((test_x, test_y, test_segmap))
 
     @staticmethod
     def _get_img_labels(labels_dir, img_list):
         lbl_columns = ['ClSph','ClDk','ClIr','ClPS','ClUn']
 
-        labels = os.path.join(labels_dir, 'labels.csv')
+        label_file = os.path.join(labels_dir, 'labels.csv')
+        labels = pd.read_csv(label_file)
+        
 
         label_list = []
         for i in img_list:
+            i = i.split('/')[-1]
             lbl_id = 'GDS_{}'.format(i.replace('.fits', ''))
             lbl = labels.loc[labels['ID']==lbl_id, lbl_columns].values.flatten()
 
             s_file = 'GDS_{}_segmap.fits'.format(i.replace('.fits', ''))
-            os.path.join(labels_dir, 'segmaps', s_file)
+            s_file = os.path.join(labels_dir, 'segmaps', s_file)
 
-            label_list.append((lbl, s_file))
+            label_list.append((lbl.astype(np.float32), s_file))
 
         return label_list
 
@@ -64,23 +71,27 @@ class Dataset:
         return training_data.make_one_shot_iterator()
 
     @staticmethod
-    def tf_prep_input(dataset_item):
-        return tf.py_func(Dataset.prep_input, dataset_item, tf.float32)
+    def tf_prep_input(x, y, segmap):
+        x, y = tf.py_func(Dataset.prep_input, [x, y, segmap], (tf.float32, tf.float32))
+        x.set_shape([Dataset.IMG_IN, Dataset.IMG_IN, Dataset.IN_CHANNELS])
+        y.set_shape([Dataset.IMG_IN, Dataset.IMG_IN, Dataset.NUM_LABELS])
+        return x, y
 
     @staticmethod
-    def prep_input(data_in):
-        x_file, label = data_in
-        lbl, segmap_file = label
+    def prep_input(x_file, lbl, segmap_file):
+        x_file, segmap_file = str(x_file, 'utf-8'), str(segmap_file, 'utf-8')
 
-        x = Dataset._safe_fits_open(x_file)
+        x = Dataset._safe_fits_open(x_file).astype(np.float32)
         segmap = Dataset._safe_fits_open(segmap_file)
         img_id = int(segmap_file.split('_')[-2])
 
-        y = np.zeros([Dataset.IMG_IN, Dataset.IMG_IN, Dataset.NUM_LABELS])
+        y = np.zeros([Dataset.IMG_IN, Dataset.IMG_IN, Dataset.NUM_LABELS], dtype=np.float32)
         y[segmap==img_id] = lbl
         y[segmap!=img_id] = Dataset.BACKGROUND
 
-        return (x, y)
+        print(x.shape, y.shape)
+
+        return [x, y]
 
     @staticmethod
     def _safe_fits_open(fits_file):
@@ -90,32 +101,32 @@ class Dataset:
         return img
 
     @staticmethod 
-    def preprocess_train(data_item):
-        return Dataset._preprocess_data(data_item, True)
+    def preprocess_train(x, y):
+        return Dataset._preprocess_data(x, y, True)
 
     @staticmethod
-    def preprocess_test(data_item):
-        return Dataset._preprocess_data(data_item, False)
+    def preprocess_test(x, y):
+        return Dataset._preprocess_data(x, y, False)
 
     @staticmethod
-    def _preprocess_data(data_item, is_training):
-        x, y = data_item
-        seed = tf.random_uniform(1, maxval=1e9, dtype=tf.float32)
-        if is_training:
-            x = Dataset._augment(x, seed)
-            y = Dataset._augment(y, seed)
+    def _preprocess_data(x, y, is_training):
+        t = tf.concat([x, y], axis=-1)
         
-        x = Dataset._crop(x, is_training, seed=seed)
-        y = Dataset._crop(y, is_training, seed=seed)
+        if is_training:
+            t = Dataset._augment(t)
+
+        t = Dataset._crop(t, is_training)
+
+        x, y = t[:,:,0,tf.newaxis], t[:,:,1:]
             
         return (Dataset._stadardize(x), y)
 
     @staticmethod
-    def _augment(t, seed):
-        angle = tf.random_uniform(1, maxval=360, seed=seed)
+    def _augment(t):
+        angle = tf.random_uniform([1], maxval=360)
         t = tf.contrib.image.rotate(t, angle, interpolation='BILINEAR')
-        t = tf.image.random_flip_left_right(t, seed=seed)
-        t = tf.image.random_flip_up_down(t, seed=seed)
+        t = tf.image.random_flip_left_right(t)
+        t = tf.image.random_flip_up_down(t)
 
         return t
 
@@ -127,12 +138,12 @@ class Dataset:
         return x
 
     @staticmethod
-    def _crop(t, is_training, seed=None):
+    def _crop(t, is_training):
         if is_training:
             pad_v = Dataset.IMG_IN + Dataset.PRE_PAD
             out_shape = [Dataset.IMG_OUT, Dataset.IMG_OUT, Dataset.IN_CHANNELS]
             t = tf.image.resize_image_with_crop_or_pad(t, pad_v, pad_v)
-            t = tf.random_crop(t, out_shape, seed=seed)
+            t = tf.random_crop(t, out_shape)
         else:
             t = tf.image.central_crop(t, Dataset.IMG_OUT/Dataset.IMG_IN)
 
@@ -140,8 +151,8 @@ class Dataset:
 
 
 def main():
-    data_dir = '~/Documents/astro_data/our_images'
-    label_dir = '~/Documents/galaxy-classification/data/labels'
+    data_dir = '../data/imgs'
+    label_dir = '../data/labels'
 
     dataset = Dataset(data_dir, label_dir)
 
