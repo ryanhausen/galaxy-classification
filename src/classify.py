3a19d1aa46ecce368f184a13c56a761962e73b98
Ryan Hausen <ryan.hausen@gmail.com>
2018-06-20 09:41:40 -0700

    WIP on classifier.py

--- src/classify.py
+++ src/classify.py
@@ -8,12 +8,32 @@ from astropy.io import fits
 # http://people.ds.cam.ac.uk/fanf2/hermes/doc/antiforgery/stats.pdf
 # http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.214.8508&rep=rep1&type=pdf
 
+# Working with large fits files
+# http://docs.astropy.org/en/stable/generated/examples/io/skip_create-large-fits.html
 
 def classify_img(*ignore, h='h.fits', j='j.fits', v='v.fits', z='z.fits'):
     naxis1, naxis2 = _validate_args(ignore, h, j, v, z)
 
-    # create placeholder fits files
+    # create fits files to fill with values
+    sph_m = 'spheroid_mean.fits'
+    sph_v = 'spheroid_var.fits'
+    dk_m = 'disk_mean.fits'
+    dk_v = 'disk_var.fits'
+    irr_m = 'irregular_mean.fits'
+    irr_v = 'irregular_var.fits'
+    ps_m = 'point_source_mean.fits'
+    ps_v = 'point_source_var.fits'
+    bkg_m = 'background_mean.fits'
+    bkg_v = 'background_var.fits'
+    n = 'n.fits'
 
+    f_names = [sph_m, sph_v, dk_m, dk_v, irr_m, irr_v, ps_m, ps_v, bkg_m, bkg_v]
+    for f in f_names:
+        _create_file(f, naxis1, naxis2, np.float32)
+
+    
+
+    
 
 
 
@@ -60,6 +80,30 @@ def _iterative_variance(n, prev_var, x_n, prev_mean, curr_mean, unbiased_est):
         return 1/(n-1) * var
     else:
         return var
+
+# http://docs.astropy.org/en/stable/generated/examples/io/skip_create-large-fits.html
+def _create_file(f_name, naxis1, naxis2, dtype):
+    stub = np.zeros([100,100], dtype=dtype)
+
+    hdu = fits.PrimaryHDU(data=stub)
+    header = hdu.header
+    while len(header) < (36 * 4 - 1):
+        header.append()
+    header['NAXIS1'] = naxis1
+    header['NAXIS2'] = naxis2
+    header.tofile(f_name)
+
+    if dtype==np.uint8:
+        bytes_per_value = 1
+    elif dtype==np.float32:
+        bytes_per_value = 4
+    elif dtype==np.float64:
+        bytes_per_value = 8
+
+    with open(f_name, 'rb+') as f:
+        f.seek(len(header.tostring()) + (naxis1 * naxis2 * bytes_per_value) - 1)
+        f.write(b'\0')
+
 
 
 if __name__=='__main__':
